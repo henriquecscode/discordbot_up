cce8b518cd088aae3abdd78682f678fa3216905a
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-10-25 14:52:30 +0100

    sql schedule database

---
+++ src/database/dbs/create_db_sqlite3.sql
@@ -0,0 +1,125 @@
+
+--
+-- Database: `tts` FOR SQLITE 3 
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `acronym` varchar(10) PRIMARY KEY ,
+  `name` text,
+  `last_updated` datetime NOT NULL
+);
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `faculty_id` varchar(10) NOT NULL,
+  `sigarra_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `acronym` varchar(10) NOT NULL,
+  `course_type` varchar(2) NOT NULL,
+  `year` int(11) NOT NULL,
+  `url` varchar(2000) NOT NULL,
+  `plan_url` varchar(2000) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  FOREIGN KEY (`faculty_id`) REFERENCES `faculty`(`acronym`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_unit`
+--
+
+CREATE TABLE `course_unit` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `sigarra_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `acronym` varchar(16) NOT NULL,
+  `url` varchar(2000) NOT NULL,
+  `semester` tinyint(4) NOT NULL,
+  `year` smallint(6) NOT NULL,
+  `schedule_url` varchar(2000) DEFAULT NULL,
+  `last_updated` datetime NOT NULL,
+  FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_metadata`
+--
+
+CREATE TABLE `course_metadata` (
+  `course_id` int(11) NOT NULL,
+  `course_unit_id` int(11) NOT NULL,
+  `course_unit_year` tinyint(4) NOT NULL,
+  `ects` float(4) NOT NULL,
+  PRIMARY KEY (`course_id`, `course_unit_id`, `course_unit_year`),
+  FOREIGN KEY (`course_unit_id`) REFERENCES `course_unit`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `day` tinyint(3) NOT NULL,
+  `duration` decimal(3,1) NOT NULL,
+  `start_time` decimal(3,1) NOT NULL,
+  `location` varchar(31) NOT NULL,
+  `lesson_type` varchar(3) NOT NULL,
+  `is_composed` boolean NOT NULL,
+  `professor_sigarra_id` int (11),
+  `course_unit_id` int(11) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  `class_name` varchar(31) NOT NULL,
+  `composed_class_name` varchar(16) DEFAULT NULL,
+  FOREIGN KEY (`course_unit_id`) REFERENCES `course_unit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- -------------------------------------------------------- 
+
+--
+-- Table structure for table `schedule_professor`
+--
+
+CREATE TABLE `schedule_professor` (
+  `schedule_id` INTEGER NOT NULL,
+  `professor_sigarra_id` INTEGER NOT NULL,
+  FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (`schedule_id`, `professor_sigarra_id`)
+);
+
+-- -------------------------------------------------------- 
+
+--
+-- Table structure for table `professor`
+--
+
+CREATE TABLE `professor` (
+  `sigarra_id` INTEGER PRIMARY KEY,
+  `professor_acronym` varchar(16),
+  `professor_name` varchar(50)
+);
+
+-- --------------------------------------------------------
